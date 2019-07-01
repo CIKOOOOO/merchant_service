e61ef9fc7379cd20cd014fbce41ac9f16ce981d5
Andrew <abedcreen@gmail.com>
2019-07-01 09:05:52 +0700

    Initial

--- app/src/main/java/com/andrew/prototype/Fragment/HomeFragment.java
+++ app/src/main/java/com/andrew/prototype/Fragment/HomeFragment.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.andrew.prototype.Adapter.HomeAdapter;
 import com.andrew.prototype.Model.Store;
@@ -17,7 +20,7 @@ import com.andrew.prototype.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private View v;
     private Context mContext;
@@ -46,14 +49,26 @@ public class HomeFragment extends Fragment {
         HomeAdapter homeAdapter = new HomeAdapter(mContext, storeList);
 
         RecyclerView recycler_home = v.findViewById(R.id.recycler_home);
-        RecyclerView recycler_ads = v.findViewById(R.id.recycler_ads_home);
+        ImageButton image_profile = v.findViewById(R.id.image_profile_home);
 
         setRecyclerAdapter(recycler_home, homeAdapter);
-//        setRecyclerAdapter(recycler_ads,);
+        image_profile.setOnClickListener(this);
     }
 
     private void setRecyclerAdapter(RecyclerView recyclerView, RecyclerView.Adapter adapter) {
         recyclerView.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false));
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.image_profile_home:
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+                fragmentTransaction.replace(R.id.main_frame, new Profile());
+                fragmentTransaction.commit();
+                break;
+        }
     }
 }
